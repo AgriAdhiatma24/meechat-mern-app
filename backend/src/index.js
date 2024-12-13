4e6ba145cfc15a0fe7801fdadacdf68ac1c77840
unknown <agriadhiatma@gmail.com>
2024-12-13 13:21:43 +0700

    Deployment preparation

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,27 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
+
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import path from "path";
 
-const app = express();
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
 dotenv.config();
+app;
+
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http:localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
@@ -22,7 +29,15 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   connectDB();
 });
